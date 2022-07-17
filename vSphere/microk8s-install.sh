82f842d6f242c075eaba715d114ac5d8fc4b9450
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2022-07-16 17:35:37 -0700

    Create microk8s-install.sh

---
+++ vSphere/microk8s-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+exec >install_microk8s.log
+exec 2>&1
+export adminUsername='boor'
+echo "###########################################################################"
+echo "Installing snap and Microk8s..." 
+echo "###########################################################################"
+sudo apt-get update
+sudo apt install snapd
+sudo snap install microk8s --classic --channel=1.24/stable
+sudo snap alias microk8s.kubectl kubectl
+sudo microk8s status --wait-ready
+echo "###########################################################################"
+echo "Microk8s specific configurations..." 
+echo "###########################################################################"
+sudo microk8s enable dns storage dashboard ingress rbac registry:size=100Gi
+# Wait until Microk8s features are done enabling
+sleep 10
+# Enable --allow-privileged for Arc Extensions deployments
+# See: https://github.com/ubuntu/microk8s/issues/749
+sudo bash -c 'echo "--allow-privileged" >> /var/snap/microk8s/current/args/kube-apiserver'
+sudo microk8s stop
+sleep 5
+sudo microk8s start
+echo "###########################################################################"
+echo "Export kubeconfig..." 
+echo "###########################################################################"
+# Set Kubeconfig - export from microk8s
+kubeconfigPath="/home/${adminUsername}/.kube"
+mkdir -p $kubeconfigPath
+sudo chown -R $adminUsername $kubeconfigPath
+sudo microk8s config view > "$kubeconfigPath/config"
